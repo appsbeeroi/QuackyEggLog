4b11c986d7c9ee870ed54dc7af25420c3086c751
esplusnin <esplusnin94@yandex.ru>
2025-08-29 22:28:31 +0300

    Initial Commit

---
+++ QuackyEggLog/QuackyEggLogApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuackyEggLogApp.swift
+//  QuackyEggLog
+//
+//  Created by Евгений on 29.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct QuackyEggLogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
